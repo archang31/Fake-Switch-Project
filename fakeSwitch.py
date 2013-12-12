bd8812adef8a916f25d8623570884e9793ec7686
Michael <mkranch@princeton.edu>
2013-12-11 21:00:40 -0500

    FakeSwitch Updates
    Running to Dinner - Working more on fakeswitch. Not sure my setup is
    right. And the point where the switch is asking the controller for
    neighbors but I sent the messages up as a pull system. Might need to go
    with something better later.

--- fakeSwitch.py
+++ fakeSwitch.py
@@ -46,23 +46,53 @@ class fakeSwitch(object):
         port = 6633
         msg_length = 12000
         print("Testing Handshake")
-        s=socket.socket()
-        s.connect((host,port))
+        self.s=socket.socket()
+        self.s.connect((host,port))
         print("Established TCP Connection")
-        msg = s.recv(74) ## s.recv in bytes
-        print("received " + binascii.hexlify(msg))
-        print("sending  " + "0100000800000002")
-        s.send(bytearray.fromhex('0100000800000002'))
-        msg = s.recv(74) ## s.recv in bytes
-        print("received " + binascii.hexlify(msg))
-        msg = binascii.hexlify(msg)
-        #outgoing = '01050008000000'
-        #outgoing = outgoing + msg[14:15]
-        #print(outgoing)
-        print("sending  " + "010600b000000013000000000000000100000100ff000000000000c700000fff0002ae2082540a8c73312d657468320000000000000000000000000000000000000000c0000000000000000000000000fffe2ef2ce7647487331000000000000000000000000000000000001000000010000000000000000000000000000000000016e2f9006b5bb73312d657468310000000000000000000000000000000001000000c0000000000000000000000000")
-        s.send(bytearray.fromhex('010600b000000013000000000000000100000100ff000000000000c700000fff0002ae2082540a8c73312d657468320000000000000000000000000000000000000000c0000000000000000000000000fffe2ef2ce7647487331000000000000000000000000000000000001000000010000000000000000000000000000000000016e2f9006b5bb73312d657468310000000000000000000000000000000001000000c0000000000000000000000000'))
-        msg = s.recv(74) ## s.recv in bytes
-        print("received " + binascii.hexlify(msg))
+        msg = self.s.recv(74) ## Hello Message
+        self.messageHandler(str(binascii.hexlify(msg)))
+        msg = self.s.recv(74) ## Features Request
+        self.messageHandler(str(binascii.hexlify(msg)))
+        msg = self.s.recv(78) ## Set Config
+        msg2 = self.s.recv(146) ## Barrier Request
+        self.messageHandler(str(binascii.hexlify(msg2)))#must respond to barrier before config
+        self.messageHandler(binascii.hexlify(msg))
+        msg = self.s.recv(90) ## Packet Out (CSM)
+        self.messageHandler(binascii.hexlify(msg))
+        msg 
+    
+    def messageHandler(self, message):
+        print(message, len(message))
+        if len(message) == 16: #8B
+            if message[0:12] == '010000080000':
+                print("Received Hello SM")
+                self.s.send(bytearray.fromhex('0100000800000002'))
+                print("Sending  Hello SM (8B)")
+            elif message[0:12] == '010500080000':
+                print("Received Features Request (SM)")
+                self.s.send(bytearray.fromhex('010600b000000013000000000000000100000100ff000000000000c700000fff0002ae2082540a8c73312d657468320000000000000000000000000000000000000000c0000000000000000000000000fffe2ef2ce7647487331000000000000000000000000000000000001000000010000000000000000000000000000000000016e2f9006b5bb73312d657468310000000000000000000000000000000001000000c0000000000000000000000000'))
+                print("Sending  Features Reply (176B)")
+            else:
+                print("ERROR: IN ELSE FOR LENGTH 16")
+        elif len(message) == 24:
+            #if message[0:14] == '0109000c000000' and message[16:24] == '00000080':
+                #three messages 0109000c0000006e00000080
+                #               0109000c0000007300000080
+                #               0109000c0000007800000080
+            print("Set Config (CSM) (12B)")
+            self.s.send(bytearray.fromhex('010a006c0000000000000100005a000200003333000000167a8ebe84fa1286dd600000000024000100000000000000000000000000000000ff0200000000000000000000000000163a000502000001008f0074f30000000104000000ff0200000000000000000001ff84fa12'))
+            print("Sending  Packet In (AM) (108B) Multicast Listen Support Message V2")
+        elif len(message) == 160:
+            # example barrier:
+            # ")#010e0048000000880010001f00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000003000000008000ffffffffffff00000112000800000089
+            print("Received Barrier Request (CSM) 8B")
+            reply = '011300080000' + message[156:160]#last 4 digits need to be the same from the barrier message
+            print(reply)
+            self.s.send(bytearray.fromhex(reply))
+            print("Sending  Barrier Reply (8B)")
+                
+        else:
+            print("In else statement")
 
 if __name__ == '__main__':
     """
