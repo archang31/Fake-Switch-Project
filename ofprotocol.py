267c1a8cf5e7ad6865fff32871ffa39ea128a5d9
Bonnie <bonnie.eisenman@gmail.com>
2013-12-27 18:21:30 -0500

    added protocol basics

---
+++ ofprotocol.py
@@ -0,0 +1,25 @@
+import struct
+
+def getHeader(msgtype, length):
+	'''Header on all OpenFlow packets.
+
+	struct ofp_header {
+		uint8_t version;
+		uint8_t type;
+		uint16_t length;
+		uint32_t xid;
+	};
+	OFP_ASSERT(sizeof(struct ofp_header) == 8);
+
+	'''
+	version = 0x02
+	xid = 0x01
+	# packet = struct.pack('!BBHI', version, msgtype, length, xid)
+	packet = struct.pack('<IHBB', xid, length, msgtype, version)
+	print len(packet)
+	return packet
+
+def getHello():
+	helloType = 0x00
+	headerLen = 0x08
+	return getHeader(helloType, headerLen)
