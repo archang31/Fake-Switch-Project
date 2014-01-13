166bc131da0c4b680408d3cf0626e16dd987f959
Michael <mkranch@princeton.edu>
2014-01-13 14:00:07 -0500

    Minor Changes to Sync
    Moved my switch into MichaelFakeSwitch to avoid issues with Bonnie’s

--- MichaelFakeSwitch.py
+++ MichaelFakeSwitch.py
@@ -12,14 +12,8 @@
 ######################################################################
 
 
-import socket
-import sys
-import dpkt
-import binascii
-import bitarray
-import time
+import binascii, bitarray, dpkt, socket, sys, threading, time
 import ofprotocol
-import threading
 
 
 class fakeSwitch(threading.Thread):
@@ -32,11 +26,8 @@ class fakeSwitch(threading.Thread):
     docstring
     """
     self.sleeptime = 0
-    #self.open_TCP_Connection()
-    #self.answer_initial_config_request() # method handles switch initial requests
-    #self.request_switch_neighbors() Not needed for setup
 
-  def run(self):
+  def run(self): #this is what is run when you do thread.start()
     if self.option == 1:
       self.packetInTest # method takes care of echo requests for keep alives
     elif self.option == 2:
@@ -52,7 +43,6 @@ class fakeSwitch(threading.Thread):
 
   def setConnection(self, connection):
     self.s = connection
-    #self.answer_initial_config_request() 
 
   def open_TCP_Connection(self):
     host = '127.0.0.1'
@@ -76,7 +66,8 @@ class fakeSwitch(threading.Thread):
       self.s.send(ofprotocol.getHello(xid))
 
     elif msgtype is ofprotocol.messageStringToType('FEATURES_REQUEST'):
-      self.s.send(ofprotocol.getFeaturesReply(xid))
+      #self.s.send(ofprotocol.getFeaturesReply(xid))
+      self.s.send(bytearray.fromhex('010600b000000013000000000000000100000100ff000000000000c700000fff0002ae2082540a8c73312d657468320000000000000000000000000000000000000000c0000000000000000000000000fffe2ef2ce7647487331000000000000000000000000000000000001000000010000000000000000000000000000000000016e2f9006b5bb73312d657468310000000000000000000000000000000001000000c0000000000000000000000000'))
 
     elif msgtype is ofprotocol.messageStringToType('ECHO_REQUEST'):
       echo_reply_header = ofprotocol.getHeader(ofprotocol.messageStringToType['ECHO_REPLY'],
@@ -97,36 +88,6 @@ class fakeSwitch(threading.Thread):
 
     elif msgtype is ofprotocol.messageStringToType('FLOW_MOD'):
       msg = "011101ac" + str(xid) + "000400000003000000000000000000000000000200000000000000070000000000000094000000000000023e0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000020000000000000000000000000003000000000000000600000000000000ee00000000000001e400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000fffe00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000003000000000000000600000000000000ee00000000000001e400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
-
-  def answer_initial_config_request(self):
-    self.eatMessage() ## Hello Message
-    self.eatMessage() ## Features Request Message
-    self.eatMessage() ## Set Config Message (no response needed)
-    #self.eatMessage() ## Flow Mod Request
-    #self.eatMessage() ## Barrier Request
-    #not sure why I could not get your barrier request to be right so I just went back to using my default.
-    #your eatMessage actually makes it look like its a Flow Mod Request with lenght 72 followed by a Barrier Request when,
-    #according to wireshark, its just a barrier Request after the set config message.
-    msg2 = self.s.recv(146) ## Barrier Request
-    #this message is not needed for a ryu controller
-    self.messageHandler(msg2) ## Barrier Reply
-    #self.eatMessage() #Get Config Request
-    #self.s.send(bytearray.fromhex('0108000c010b5e800000ffff')) #Get Config Reply
-    #real switch reply Header = 01 08 00 0c
-    #second byte is "transaction ID "00 b6  8c 98 ''    00 00 ff ff 
-
-    #this message is not sent on a ryu controller
-    
- #   msg = self.s.recv(74) ## Hello Message
- #   self.genericMessageHandler(msg)
- 
-    #self.messageHandler(msg)
- #   msg = self.s.recv(74) ## Features Request
- #   self.genericMessageHandler(msg)
-    #self.messageHandler(msg)
- #   msg = self.s.recv(78) ## Set Config Message (no response needed)
-    # msg2 = self.s.recv(146) ## Barrier Request
-    # self.messageHandler(msg2) ## Barrier Reply
   
   def echo_loop(self):
     while(1):
@@ -145,49 +106,6 @@ class fakeSwitch(threading.Thread):
       self.s.send(bytearray.fromhex('010a006c0000000000000107005a000100003333000000166ae6be545fdd86dd6000000000240001fe8000000000000068e6befffe545fddff0200000000000000000000000000163a000502000001008f008abf0000000104000000ff0200000000000000000001ff545fdd'))
       #print("Sending  Second Packet In - Router Soliciation")
       self.s.recv(90) ## Packet Out (CSM) Respond
-      
-  def messageHandler(self, msg):
-    message = str(binascii.hexlify(msg))
-    #print(message, len(message))
-    #print msg, len(msg)
-    header = ofprotocol.deserializeHeader(msg[:8])
-    (version, msgtype, length, xid) = header
-    #print 'messageHandler: deserializeHeader: ' + str(header)
-
-    if len(message) == 16: #8B
-
-      if message[0:12] == '010000080000':
-        print("Received Hello SM")
-        self.s.send(ofprotocol.getHello())
-        #self.s.send(bytearray.fromhex('0100000800000002'))
-        print("Sending  Hello SM (8B)")
-
-      elif message[0:12] == '010500080000':
-        print("Received Features Request (SM)")
-        self.s.send(bytearray.fromhex('010600b000000013000000000000000100000100ff000000000000c700000fff0002ae2082540a8c73312d657468320000000000000000000000000000000000000000c0000000000000000000000000fffe2ef2ce7647487331000000000000000000000000000000000001000000010000000000000000000000000000000000016e2f9006b5bb73312d657468310000000000000000000000000000000001000000c0000000000000000000000000'))
-        print("Sending  Features Reply (176B)")
-
-      else:
-        print("ERROR: IN ELSE FOR LENGTH 16")
-
-    elif len(message) == 24:
-      #if message[0:14] == '0109000c000000' and message[16:24] == '00000080':
-          #three messages 0109000c0000006e00000080
-          #               0109000c0000007300000080
-          #               0109000c0000007800000080
-      print("Set Config (CSM) (12B)")
-      print("I do not believe a response is needed")
-
-    elif len(message) == 160:
-      # example barrier:
-      # ")#010e0048000000880010001f00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000003000000008000ffffffffffff00000112000800000089
-      print("Received Barrier Request (CSM) 8B")
-      reply = '011300080000' + message[156:160]#last 4 digits need to be the same from the barrier message
-      self.s.send(bytearray.fromhex(reply))
-      print("Sending  Barrier Reply (8B)")
-            
-    else:
-      print("In else statement from message handler - should not be here")
 
   def request_switch_neighbors(self):
     self.s.send(bytearray.fromhex('010a006c0000000000000100005a000200003333000000167a8ebe84fa1286dd600000000024000100000000000000000000000000000000ff0200000000000000000000000000163a000502000001008f0074f30000000104000000ff0200000000000000000001ff84fa12'))
